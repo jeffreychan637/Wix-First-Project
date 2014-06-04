b503bdbbabada191ef41940602a8589b8229c404
Jeffrey Chan <jeffrey@wix.com>
2014-06-04 12:10:13 -0700

    Changed server to use Heroku servers when possible and 3000 otherwise

--- WixBackend/server.js
+++ WixBackend/server.js
@@ -2,7 +2,7 @@
 
 var app = require('./app');
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('listening to port %d', server.address().port);
 });
 
